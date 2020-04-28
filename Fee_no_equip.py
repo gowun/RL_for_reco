5a4ee4f2ac467219fd07a629d4980b5494497f9d
kennie <gowun.jeong@gmail.com>
2020-04-28 09:31:48 +0900

    update fee_no_equip

--- Fee_no_equip.py
+++ Fee_no_equip.py
@@ -6,29 +6,20 @@ from mushroom_rl.environments import Environment, MDPInfo
 from mushroom_rl.utils import spaces
 
 class Fee_no_equip(Environment):
-    def __init__(self, ug_labels, fb_labels, gamma, horizon, sas_abs_path, knn_model_abs_path, ug_model_abs_path):
+    def __init__(self, fb_labels, gamma, horizon, min_value, max_value, state_mu_sigma_abs_path, trans_model_abs_path, reward_model_abs_path):
         # MDP parameters
-        self.ug_labels = ug_labels   ## user group
         self.fb_labels = fb_labels   ## fee block
         self.gamma = gamma    ## discount factor
         self.horizon = horizon    ## time limit to long
-        """
-        sas_dataset: state-action-state, pandas dataframe
-        - state: cluster membership + score of actions
-        - action: fee block of price plans according to their fees
-
-        state / given action / next state
-        """
-        self.sas_dataset = pickle.load(open(sas_abs_path, 'rb'))
-        self.current_cols = list(filter(lambda x: x.endswith('_next') == False and x.startswith('rec_') == False, self.sas_dataset.columns))
-        self.next_cols = list(filter(lambda x: 'next' in x, self.sas_dataset.columns))
-        self.knn_model = pickle.load(open(knn_model_abs_path, 'rb')) ## ug 별로 존재, two components of model and mapping from associate to original indexes
-        self.ug_model = pickle.load(open(ug_model_abs_path, 'rb'))  ## clustering
-        #self.score_model = pickle.load(open(score_model_abs_path, 'rb'))  ## multi-classification 
+        
+        self.state_mu_sigma = pickle.load(open(state_mu_sigma_abs_path, 'rb'))
+        self.n_state_dim = len(self.state_mu_sigma)
+        self.trans_model = pickle.load(open(trans_model_abs_path, 'rb'))  ## state transition model
+        self.reward_model = pickle.load(open(reward_model_abs_path, 'rb'))
 
         ## ug, scores of fb classes
-        self.min_point = np.zeros(len(self.ug_labels) + len(self.fb_labels))
-        self.max_point = np.ones(len(self.ug_labels) + len(self.fb_labels))
+        self.min_point = np.ones(self.n_state_dim) * min_value
+        self.max_point = np.ones(self.n_state_dim) * max_value
 
         self.fee_base = np.array(list(map(lambda x: float(x[3]), self.fb_labels)))
         self.max_reward = 10.0
@@ -44,40 +35,23 @@ class Fee_no_equip(Environment):
     def reset(self, state=None):
         if state is None:
             np.random.seed()
-            idx = np.random.choice(len(self.sas_dataset))
-            self._state = np.array(self.sas_dataset.values[idx][:len(self.min_point)])
+            self._state = np.array(list(map(lambda x: np.random.normal(x[0], x[1]), self.state_mu_sigma)))
         else:
             self._state = np.array(state)
         return self._state
 
-    def _estimate_current_state(self, model_idx, a):
-        idxs = self.knn_model[model_idx]['model'].kneighbors([self._state[len(self.ug_labels):]], self.knn_model[model_idx]['size'], return_distance=False)[0]
-        idxs = list(map(lambda x: self.knn_model[model_idx]['mapping'][x], idxs))
-        return self.sas_dataset.iloc[idxs].query(f"rec_{a} > 0.0")
+    def _find_next_state(self, action_onehot, ith_next):
+        s = np.concatenate([self._state, action_onehot])
+        return self.trans_model[ith_next].predict([s])[0]
+        
+    def _find_reward(self, action_onehot, next_state):
+        s = np.concatenate([self._state, action_onehot, next_state])
+        return self.reward_model.predict([s])[0]
 
     def step(self, action):
-        ### find the most similar case in sas_dataset
-        model_idx = list(self._state[:len(self.ug_labels)]).index(1.0)
-        a = self.fb_labels[int(action)]
-        pos_states = self._estimate_current_state(model_idx, a)
-        if len(pos_states) > 0:
-            next_state = np.array(pos_states[self.next_cols].values[0])
-        else:
-            for cl in self.ug_model['order'][model_idx]:
-                pos_states = self._estimate_current_state(cl, a)
-                if len(pos_states) > 0:
-                    next_state = np.array(pos_states[self.next_cols].values[0])
-                    break
-
-        fa = float(a[3])
-        """
-        weight rule
-        1) 가장 비싼 요금제로 이동, max_reward
-        2) 추천한 요금제 이상의 요금제로 이동, 요금제 최대값 + 1, 즉 7
-        3) 추천한 요금제 미만의 요금제로 이동, 이동한 요금제 - 추천한 요금제
-        """
-        weights = list(map(lambda x: self.max_reward if x == max(self.fee_base) else (max(self.fee_base) + 1 if x >= fa else x - fa), self.fee_base))
-        ## -1.8 < reward < 1.9
-        reward = sum(np.array(next_state[len(self.ug_labels):]) * np.array(weights)) ** 1/len(self.fb_labels)
+        action_onehot = np.array([False] * len(self.fb_labels))
+        action_onehot[action] = True
+        next_state = np.array(list(map(lambda x: self._find_next_state(action_onehot, x), range(self.n_state_dim))))
+        reward = self._find_reward(action_onehot, next_state)
         
         return next_state, reward, False, {}
