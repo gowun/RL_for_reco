35d1561a6f92f30a34c61a955e35147fa436bfd8
gowun.jeong <gowun.jeong@gmail.com>
2020-05-07 12:25:31 +0900

    revisie

--- Network_for_Reco.py
+++ Network_for_Reco.py
@@ -8,11 +8,11 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 class Network_for_Reco(nn.Module):
-    def __init__(self, in_dim, hidden_dims, out_dim, **kwargs):
+    def __init__(self, input_shape, output_shape, hidden_dims, **kwargs):
         super().__init__()
 
         self.fully_connected_net = []
-        in_size = in_dim
+        in_size = input_shape[-1]
         for i, next_size in enumerate(hidden_dims):
             hh = nn.Linear(in_features=in_size, out_features=next_size)
             nn.init.xavier_uniform_(hh.weight, gain=nn.init.calculate_gain('relu'))
@@ -20,7 +20,7 @@ class Network_for_Reco(nn.Module):
             self.__setattr__('_h{i}'.format(i), hh)
             self.fully_connected_net.append(hh)
 
-        self.last_layer = nn.Linear(in_features=in_size, out_features=out_dim)
+        self.last_layer = nn.Linear(in_features=in_size, out_features=output_shape[0])
         nn.init.xavier_uniform_(self.last_layer.weight, gain=nn.init.calculate_gain('linear'))
 
     def forward(self, state, action=None):
