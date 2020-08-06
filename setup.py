138da2018241b10d68c1721bc08c7bf2747e6eda
JeongGowun <jeonggowun@ByeongYeolui-MacBookAir.local>
2020-08-06 15:02:27 +0900

    setup change

--- setup.py
+++ setup.py
@@ -5,22 +5,6 @@ from RL_for_reco import __version__
 
 here = path.abspath(path.dirname(__file__))
 
-requires_list = [
-        "thrift-sasl>=0.3.0", 
-        "hvac>=0.9.6",
-        "pyhive[hive]",
-        "pyarrow>=0.16.0",
-        "pandas>=1.0.3",
-        "slackclient>=2.5.0",
-        "google-cloud-bigquery>=1.24.0",
-        "httplib2>=0.18.0",
-        "click",
-        "PyGithub",
-        "pycryptodome",
-        "joblib>=0.14.1",
-        "tabulate>=0.8.7",
-]
-
 air = [
     "torch>=1.4.0",
     "torchvision>=0.5.0",
@@ -28,6 +12,7 @@ air = [
     "scipy>=1.4.1",
     "matplotlib>=3.2.1",
     "mushroom_rl>=1.4.0",
+    "joblib>=0.14.1",
 ]
 
 
@@ -43,10 +28,9 @@ setuptools.setup(
     python_requires=">=3.6",
     zip_safe=False,
     long_description=open('README.md').read(),
-    install_requires=requires_list,
+    install_requires=air,
     classifiers=["Programming Language :: Python :: 3",
                  "License :: OSI Approved :: MIT License",
                  "Operating System :: OS Independent",
                  ],
-    extras_require={"air": air},
 )
