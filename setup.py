929f7d26dc39d4cacdbecaac0adb67c6422aec59
gowun <gowun.jeong@gmail.com>
2020-08-06 14:38:43 +0900

    Update setup.py
    버전 업..

--- setup.py
+++ setup.py
@@ -6,28 +6,28 @@ from RL_for_reco import __version__
 here = path.abspath(path.dirname(__file__))
 
 requires_list = [
-        "thrift-sasl==0.3.0", 
+        "thrift-sasl>=0.3.0", 
         "hvac>=0.9.6",
         "pyhive[hive]",
         "pyarrow>=0.16.0",
-        "pandas==1.0.3",
+        "pandas>=1.0.3",
         "slackclient>=2.5.0",
         "google-cloud-bigquery>=1.24.0",
         "httplib2>=0.18.0",
         "click",
         "PyGithub",
         "pycryptodome",
-        "joblib==0.14.1",
+        "joblib>=0.14.1",
         "tabulate>=0.8.7",
 ]
 
 air = [
-    "torch==1.4.0",
-    "torchvision==0.5.0",
-    "scikit-learn==0.22.2.post1",
-    "scipy==1.4.1",
-    "matplotlib==3.2.1",
-    "mushroom_rl==1.4.0",
+    "torch>=1.4.0",
+    "torchvision>=0.5.0",
+    "scikit-learn>=0.22.2.post1",
+    "scipy>=1.4.1",
+    "matplotlib>=3.2.1",
+    "mushroom_rl>=1.4.0",
 ]
 
 
